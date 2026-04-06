f6c1ac3a1dbd3020e634fce6674af42bdf37edd7
aiobsessed <68997822+aiobsessed@users.noreply.github.com>
2026-04-06 23:47:46 +0300

    feat: add Alembic migrations and initial DB schema

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 from contextlib import asynccontextmanager
+
+from alembic import command
+from alembic.config import Config
 from fastapi import FastAPI
+
 from app.database.database import db
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await db.create_database_if_not_exist()
+
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
+
     yield
     await db.dispose()
 
