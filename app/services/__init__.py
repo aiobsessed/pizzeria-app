ac435030b68b48e45ff441f2d48c12c26f7fc1a7
aiobsessed <68997822+aiobsessed@users.noreply.github.com>
2026-04-29 01:09:30 +0300

    chore: add services __init__.py

---
+++ app/services/__init__.py
@@ -0,0 +1,7 @@
+from .address import AddressService
+from .cart import CartService
+from .category import CategoryService
+from .courier import CourierService
+from .order import OrderService
+from .product import ProductService
+from .user import UserService
